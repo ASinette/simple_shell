40e9b71535bb85da441d7f333afb7ac36aee89af
Géradine Sinette AKOUTEY <105546591+ASinette@users.noreply.github.com>
2022-08-13 01:04:52 +0100

    Update string_functions.c

--- string_functions.c
+++ string_functions.c
@@ -1,145 +1,98 @@
 #include "shell.h"
 
 /**
- * _genWord - returns word
- * @Cptr: char pointer
- * @abc: start of the word
- * @len: length of the word
- *
- * Return: Word number
- */
+* _strncpy - Function that copies a string into other
+*@dest: destination of the string
+*@src: string to copy
+*@n: length of the string
+*Return: dest
+*/
 
-char *_genWord(char *Cptr, int abc, int len)
+char *_strncpy(char *dest, char *src, int n)
 {
-char *x;
-int i;
+	int i;
 
-x = malloc(sizeof(char) * len + 1);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-if (x == NULL)
-return (NULL);
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
-for (i = 0; i < (len); i++)
-x[i] = Cptr[abc + i];
+	return (dest);
+}
 
-x[i] = '\0';
 
-return (x);
+/**
+* _strncpyconst - Function that copies a constant string into other
+*@dest: destination of the string
+*@src: string to copy
+*@n: length of the string
+*Return: dest
+*/
+
+char *_strncpyconst(char *dest, const char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
 
 /**
- * _Word - add a word to an array
- * @ptrW: word to add
- * @ptrI: current index
- * @Arr: array
+ * _strlen - it gives the length of a string
+ * @s: pointer to the string
+ * Return: the length of string
  */
-
-void _Word(char *ptrW, int *ptrI, char **Arr)
+int _strlen(char *s)
 {
-ptrW = _cleanString(ptrW);
-Arr[*ptrI] = _strdup(ptrW);
-free(ptrW);
-*ptrI += 1;
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
- * _cleanString - erase begin spaces and ending spaces from string
- *
- * @prmString: string to clean
- *
- * Return: clean string
- */
-char *_cleanString(char *prmString)
+* _strlen_const - Function to find the length of a constant string
+*@str: string to calculate the length
+*Return: the length of the string
+*/
+
+unsigned int _strlen_const(const char *str)
 {
-	int iLoop = 0, NLoop = 0, size = 0;
-	char *string;
+	unsigned int i = 0;
 
-	if (prmString == NULL)
-		return (NULL);
+	while (str[i] != '\0')
+		i++;
 
-	size = _strlen(prmString);
-
-	while (_Arr(prmString[iLoop], " ") == 1)
-		iLoop++;
-
-	NLoop = size - 1;
-	while (_Arr(prmString[NLoop], " ") == 1)
-		NLoop--;
-	NLoop = size - 1 - NLoop;
-
-	string = _calloc(sizeof(char), size - iLoop - NLoop + 1);
-	strncpy(string, (prmString + iLoop), size - iLoop - NLoop);
-	string[size - iLoop - NLoop] = '\0';
-
-	/**prmString = *string;*/
-	prmString = _realloc(
-		prmString,
-		size,
-		sizeof(char) * (size - iLoop - NLoop + 1)
-	);
-	_strcpy(prmString, string);
-	prmString[size - iLoop - NLoop] = '\0';
-	free(string);
-	return (prmString);
+	return (i);
 }
+
+
 
 /**
- * _help - manage command help
- *
- * @prmData: data structure
- */
-void _help(server_t *ptrData)
+* _strcmp - Function to compare 2 strings and find if are equal
+*@s1: first string to compare
+*@s2: second string to compare
+*Return: 1 for equal, 0 if not
+*/
+
+int _strcmp(char *s1, char *s2)
 {
-if (ptrData == NULL)
-return;
-if (ptrData->cmdName == NULL)
-return;
-    if (
-		ptrData->argument == NULL ||
-		ptrData->argument[0] == NULL ||
-		ptrData->argument[1] == NULL
-	)
-return;
-if (_strcmp(ptrData->argument[1], "cd") == 0)
-_changedirectoryHelp();
-else if (_strcmp(ptrData->argument[1], "env") == 0)
-_envHelp();
-else if (_strcmp(ptrData->argument[1], "setenv") == 0)
-return;
-else if (_strcmp(ptrData->argument[1], "unsetenv") == 0)
-return;
-else if (_strcmp(ptrData->argument[1], "exit") == 0)
-_exitHelp();
-else if (_strcmp(ptrData->argument[1], "help") == 0)
-_helpHelp();
-else
-_defaultHelp(ptrData->argument[1]);
-}
+	unsigned int i = 0;
 
-/**
- * _exits - exit status
- *
- * @ptrData: data structure
- */
-void _exits(server_t *ptrData)
-{
-int input = EXIT_SUCCESS;
+	while (s1[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (0);
+		i++;
+	}
 
-if (ptrData == NULL)
-return;
-
-if (ptrData->argument != NULL)
-input = _atoi(ptrData->argument[1]);
-
-if (input < 0 || (ptrData->argument[1] != NULL && !_isNumber(ptrData->argument[1])))
-{
-_Error(ptrData, 106);
-input = 2;
-}
-
-freeServerData(ptrData);
-freeList(ptrData->env);
-ptrData->env = NULL;
-free(ptrData);
-exit(input);
+	return (1);
 }
