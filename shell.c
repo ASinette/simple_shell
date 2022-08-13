4f240d7db78391049aa38ab1a4287b01afb461b8
Géradine Sinette AKOUTEY <105546591+ASinette@users.noreply.github.com>
2022-08-13 01:03:21 +0100

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,118 +1,137 @@
 #include "shell.h"
 
-void _Error(server_t *ptrData, int m)
+/**
+ * prompt - display prompt
+ *
+ * Return: void
+ */
+void prompt(void)
 {
-	
-int i = 0;
+	char *str = "$ ";
 
-Error_t errors[] = {
-{00, "Error unknown"},
-{100, "Error reading from Prompt - Zero tokens"},
-{101, "No such file or directory"},
-{102, "FATAL ERROR: UNABLE TO CREATE CHILD PROCESS"},
-{103, "command not found"},
-{104, "FATAL ERROR: NO MEMORY AVAILABLE."},
-{105, "FATAL ERROR: UNABLE TO CREATE CMD LIST"},
-{106, "Illegal number"},
-{999, ""} /* Error Array end condition*/
-};
-
-_write(ptrData->pgName);
-_write(": ");
-_write("1");
-_write(": ");
-_write(ptrData->cmdName);
-_write(": ");
-while (i < 9)
-{
-if (m == (errors + i)->input)
-{
-_write((errors + i)->errorMessage);
-_write(": ");
-_write(ptrData->argument[1]);
-}
-i++;
-}
-_write("\n");
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, str, 2);
 }
 
 /**
- * _listEnvLen - length of a linked list
+ * parse_input - gets and parses a line from the prompt
+ * @str: line to parse
  *
- * @prmHead: first node
- *
- * Return: length
+ * Return: Array of tokens to be executed
  */
-int _listEnvLen(serverEnv_t *prmHead)
+char **parse_input(char *str)
 {
-	if (prmHead == NULL)
-		return (0);
-	if (prmHead->next == NULL)
-		return (1);
-	else
-		return (_listEnvLen(prmHead->next) + 1);
-}
+	char *token, **holder;
+	unsigned int length;
+	int i = 0;
 
-/**
- * _genGlobal - generate a global environment variable
- *                      from name and value
- * @ptrName: environment name
- * @ptrV: environment value
- *
- * Return: global environment variable
- */
-char *_genGlobal(char *ptrName, char *ptrV)
-{
-	char *tmp, *global;
-
-	global = "";
-	global = _strconcat(global, ptrName);
-	tmp = _strconcat(global, "=");
-	free(global);
-	global = _strconcat(tmp, ptrV);
-	free(tmp);
-
-	return (global);
-}
-
-/**
- * _isdigit - test if a character is a number
- *
- * @prmChar: char
- *
- * Return: return 1 if true
- */
-int _isdigit(char ptrC)
-{
-	return (ptrC >= '0' && ptrC <= '9');
-}
-
-/**
- * _numLen - length of a number
- * @Num: number
- * Return: length of the number
- */
-
-int _numLen(int Num)
-{
-	int i = 0, num1;
-
-	if (Num == 0)
-		return (1);
-
-	num1 = Num;
-
-	if (num1 < 0)
+	str[_strlen(str) - 1] = '\0'; /**replace the new line by null*/
+	length = length_of_command(str);
+	if (length == 0)
+		return (NULL);
+	holder = malloc((sizeof(char *) * (length + 1)));
+	if (holder == NULL)
+		return (NULL);
+	token = strtok(str, " ");
+	while (token != NULL)
 	{
-		num1 *= -1;
-		i++;	/* for the sign char */
-	}
-
-	while (num1)
-	{
-		num1 /= 10;
+		holder[i] = malloc(_strlen(token) + 1);
+		if (holder[i] == NULL)
+		{
+			freearv(holder);
+			return (NULL);
+		}
+		_strncpy(holder[i], token, _strlen(token) + 1);
+		token = strtok(NULL, " ");
 		i++;
 	}
+	holder[i] = NULL;
+	return (holder);
+}
 
-	return (i);
+/**
+* execute - Function to verify and execute every command
+*@commands: command get
+*@buffer: command in getline
+*@env: enviroment variable
+*@argv: argument counter
+*@count: number of times that executed
+*Return: Nothing(void)
+*/
+
+void execute(char **commands, char *buffer, char **env, char **argv, int count)
+{
+	struct stat fileStat;
+
+	/*check if command is NULL or empty spaces*/
+	if (commands == NULL)
+		null_command(buffer);
+	/*check if command is exit to exit from shell*/
+	else if (_strcmp("exit", commands[0]))
+		get_out(buffer, commands);
+	/* check if the command is ENV to print environment variables */
+	else if (_strcmp("env", commands[0]))
+		env_end(buffer, commands, env);
+	/*check if the command is a full path to an executable file*/
+	else if (stat(commands[0], &fileStat) == 0)
+		execve(commands[0], commands, NULL);
+	/*check all $PATH directories for executable commands*/
+	else
+		_path(commands, buffer, env, argv, count);
+}
+
+/**
+ * signal_handler - handles signals and write the prompt
+ * @sig: signal to handle
+ *
+ * Return: void
+ */
+void signal_handler(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n$ ", 3);
+}
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: array of arguments
+ * @env: pointer to list of env variables:wq
+ *
+ *
+ * Return: 0 on Success or 1 if Error
+ */
+int main(int __attribute__((unused)) argc, char **argv, char **env)
+{
+	size_t len = 0;
+	ssize_t chars;
+	pid_t pid;
+	char **tokens, *line = NULL;
+	int i = 0, status;
+
+	(void)argc;
+	prompt();
+	for (; (chars = getline(&line, &len, stdin)); )
+	{
+		signal(SIGINT, signal_handler); /*CTRL+C*/
+		if (chars == EOF)
+			end_file(line);
+		i++;
+		tokens = parse_input(line);
+		pid = fork();
+		if (pid == -1)
+			fork_fail();
+		if (pid == 0)
+			execute(tokens, line, env, argv, i);
+		else
+		{
+			wait(&status);
+			send_to_free(line, tokens);
+		}
+		len = 0, line = NULL;
+		prompt();
+	}
+	if (chars == -1)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
